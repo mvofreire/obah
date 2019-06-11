ceb9574f33ffdee113432cbf843bfd8de6288feb
Marcus Freire <vinny.freire@gmail.com>
2019-06-11 00:14:41 -0300

    add Hello World

--- application/server.js
+++ application/server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
 const SERVER_PORT = process.env.PORT || 5000;
 app.listen(SERVER_PORT, function() {
   console.log("ESCUTANDO NA PORTA " + SERVER_PORT);
