2651eff005866a878cd544b0e02c14b1bda732fa
Marcus Freire <vinny.freire@gmail.com>
2019-06-11 00:17:50 -0300

    add route

--- application/server.js
+++ application/server.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
+app.get("/route", function(req, res) {
+  res.send("Hello Route!");
+});
+
 const SERVER_PORT = process.env.PORT || 5000;
 app.listen(SERVER_PORT, function() {
   console.log("ESCUTANDO NA PORTA " + SERVER_PORT);
